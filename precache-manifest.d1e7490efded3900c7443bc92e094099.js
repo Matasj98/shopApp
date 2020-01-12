36b4dd56e6964a1b687d90f972423ac36009a287
Matas <matas.jarilinas@stud.vgtu.lt>
2020-01-12 18:38:26 +0200

    Updates

---
+++ precache-manifest.d1e7490efded3900c7443bc92e094099.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277472977f742a729a881cf79aea1cd2",
+    "url": "/shopApp/index.html"
+  },
+  {
+    "revision": "03e43f5d35ff31d59c32",
+    "url": "/shopApp/static/css/main.13ec8cd6.chunk.css"
+  },
+  {
+    "revision": "e34128df694ea572a962",
+    "url": "/shopApp/static/js/2.255f0bf7.chunk.js"
+  },
+  {
+    "revision": "25ab7760a9d196e1bee67b6bdb56e1bd",
+    "url": "/shopApp/static/js/2.255f0bf7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03e43f5d35ff31d59c32",
+    "url": "/shopApp/static/js/main.ef4ec335.chunk.js"
+  },
+  {
+    "revision": "0adab4bd85454870b35d",
+    "url": "/shopApp/static/js/runtime-main.cf93589b.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/shopApp/static/media/crown.ed28accb.svg"
+  }
+]);
